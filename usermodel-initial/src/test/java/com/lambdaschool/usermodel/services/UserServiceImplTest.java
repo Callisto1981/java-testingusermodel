6a3d4bca7be69685df105d245dda82039bd4d918
Wayne <wabbruscatoorr@gmail.com>
2020-09-17 16:09:55 -0400

    All tests past in UserServiceImpl test file

--- usermodel-initial/src/test/java/com/lambdaschool/usermodel/services/UserServiceImplTest.java
+++ usermodel-initial/src/test/java/com/lambdaschool/usermodel/services/UserServiceImplTest.java
@@ -1,6 +1,9 @@
 package com.lambdaschool.usermodel.services;
 
 import com.lambdaschool.usermodel.UserModelApplication;
+import com.lambdaschool.usermodel.models.Role;
+import com.lambdaschool.usermodel.models.User;
+import com.lambdaschool.usermodel.models.UserRoles;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -23,6 +26,9 @@ public class UserServiceImplTest
 {
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private RoleService roleService;
 
     @Before
     public void setUp() throws Exception
@@ -53,30 +59,61 @@ public class UserServiceImplTest
     @Test
     public void findAll()
     {
+        assertEquals(5, userService.findAll().size());
     }
 
     @Test
     public void delete()
     {
+        userService.delete(13);
+        assertEquals(4, userService.findAll().size());
     }
 
     @Test
     public void findByName()
     {
+        assertEquals(14, userService.findByName("misskitty").getUserid());
     }
 
     @Test
     public void save()
     {
+        String username = "happy";
+        Role r1 = roleService.findByName("admin");
+        Role r2 = roleService.findByName("user");
+
+        User user = new User("happy",
+            "password",
+            "happy@example.com");
+        user.getRoles().add(new UserRoles(user, r1));
+        user.getRoles().add(new UserRoles(user, r2));
+
+        User addUser = userService.save(user);
+        assertNotNull(addUser);
+        assertEquals(username, addUser.getUsername());
     }
 
     @Test
     public void update()
     {
+        String username = "happy";
+        Role r2 = roleService.findByName("user");
+
+        User user = new User("happy",
+            "password",
+            "happy@example.com");
+        user.getRoles().add(new UserRoles(user, r2));
+        user.setUserid(13);
+        User addUser = userService.update(user, 13);
+
+        assertNotNull(addUser);
+        assertEquals(username, addUser.getUsername());
     }
 
     @Test
     public void deleteAll()
     {
+        userService.deleteAll();;
+        assertEquals(0, userService.findAll().size());
     }
 }
